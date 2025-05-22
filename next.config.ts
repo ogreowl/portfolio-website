62ba6482fc7d81bf6777ce2a2e5926610b668617
Bobby Becker <beckerowl@gmail.com>
2025-05-22 18:27:13 -0500

    FINAL this time

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   productionBrowserSourceMaps: false,
-  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
